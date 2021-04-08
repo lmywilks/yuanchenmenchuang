257f0335b71bfa1f5c1f524be2f526ea90559596
lmywilks <lmywilks@hotmail.com>
2021-04-08 08:29:02 -0400

    fix api root

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_root: 'https://asia-east2-yuanchenmenchuang-app.cloudfunctions.net/api',
+  api_root: 'https://asia-east2-yuanchenmenchuang.cloudfunctions.net/api',
   storage: 'https://firebasestorage.googleapis.com/v0/b/yuanchenmenchuang.appspot.com/o/products%2F'
 };
